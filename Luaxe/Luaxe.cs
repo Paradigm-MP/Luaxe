dd210dba34e6dd0b8f67136ec6a75ba843391d89
benank <terkolskie@gmail.com>
2021-08-28 11:05:46 -0700

    Luaxe works

---
+++ Luaxe/Luaxe.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Luaxe
+{
+    [BepInPlugin("Paradigm.Luaxe", "Luaxe", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class Luaxe : BaseUnityPlugin
+    {
+		private readonly Harmony harmony = new Harmony("Paradigm.Luaxe");
+        void Awake()
+        {
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(Player), nameof(Player.OnJump))]
+		class FixOnSwiming
+		{
+			public static void Prefix(Player __instance)
+			{
+				Player player = __instance;
+				player.m_jumpForce = 100f;
+			}
+		}
+    }
+}
